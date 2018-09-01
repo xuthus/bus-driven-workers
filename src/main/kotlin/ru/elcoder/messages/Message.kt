494ada1b41d3f847f52f7f22739f42bb0723df07
xuthus <xuthus@yandex.ru>
2018-09-01 19:57:34 +0300

    small fixes

--- src/main/kotlin/ru/elcoder/messages/Message.kt
+++ src/main/kotlin/ru/elcoder/messages/Message.kt
@@ -3,5 +3,4 @@ package ru.elcoder.messages
 data class Message(
         val messageType: MessageType,
         val source: String,
-        val payload: Any) {
-}
+        val payload: Any)
--- src/test/kotlin/ru/elcoder/HelloTest.kt
+++
@@ -1,8 +0,0 @@
-package ru.elcoder
-
-import org.junit.Test
-import kotlin.test.assertEquals
-
-class HelloTest {
-
-}
